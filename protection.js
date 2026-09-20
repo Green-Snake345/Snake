146dd035c0f919a280fa56edc0704cd857e70f01
Green Snake <108731097+Green-Snake345@users.noreply.github.com>
2026-09-21 01:35:05 +0500

    Add files via upload

---
+++ protection.js
@@ -0,0 +1,13 @@
+(function() {
+    window.addEventListener('keydown', function(event) {
+        if (event.key === "F12") {
+            event.preventDefault();
+        }
+        if (event.ctrlKey && event.shiftKey && event.key === "I") {
+            event.preventDefault();
+        }
+        if (event.ctrlKey && event.shiftKey && event.key === "J") {
+            event.preventDefault();
+        }
+    });
+})();
